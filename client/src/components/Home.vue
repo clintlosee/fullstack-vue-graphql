2ab4be64f0fa8362184ec32cbf8eb989f5e37340
Clint Losee <clint.losee@gmail.com>
2019-04-10 13:44:36 -0600

    Modified file structure. Added Vuetify. Added color theme.

---
+++ client/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Home Component</h2>
+    <v-btn color="primary"> Button</v-btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: {}
+};
+</script>
---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  theme: {
+    primary: '#EA7209',
+    secondary: '353535',
+    accent: '#F29D4D',
+    error: '9A031E',
+    warning: '#FFB400',
+    info: '#C9DAEA',
+    success: '#92EA81',
+    offwhite: '#FBF5F3',
+    alternate: '#654236',
+  },
+});
