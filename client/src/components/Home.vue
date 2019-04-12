83b1b60c0f70db5df748e4e27d378c2e0d72a212
Clint Losee <clint.losee@gmail.com>
2019-04-12 12:15:06 -0600

    Adding apollo and setting up basic query examples. Will be replaced.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,91 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <div v-if="$apollo.loading">Loading...</div>
+    <ul v-else v-for="post in getPosts" :key="post._id">
+      <li>{{ post.title }}</li>
+      <li>{{ post.imageUrl }}</li>
+      <li>{{ post.description }}</li>
+    </ul>
+
+    <v-divider></v-divider>
+
+    <ApolloQuery :query="getPostsQuery">
+      <template slot-scope="{ result: { loading, error, data } }">
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="error">ERROR {{ error.message }}</div>
+        <ul v-else v-for="post in data.getPosts" :key="post._id">
+          <li>{{ post.title }}</li>
+          <li>{{ post.imageUrl }}</li>
+          <li>{{ post.description }}</li>
+        </ul>
+      </template>
+    </ApolloQuery>
   </v-container>
 </template>
 
 <script>
+import { gql } from "apollo-boost";
+
 export default {
   name: "home",
-  components: {}
+  components: {},
+  data() {
+    return {
+      posts: [],
+      getPostsQuery: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            categories
+            description
+            createdDate
+            likes
+            createdBy {
+              _id
+              username
+              email
+              password
+              joinDate
+            }
+          }
+        }
+      `
+    };
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            categories
+            description
+            createdDate
+            likes
+            createdBy {
+              _id
+              username
+              email
+              password
+              joinDate
+            }
+          }
+        }
+      `,
+      result({ data, loading }) {
+        if (!loading) {
+          this.posts = data.getPosts;
+        }
+      },
+      error(err) {
+        console.log("ERROR:", err);
+      }
+    }
+  }
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,25 @@
-import '@babel/polyfill'
+import '@babel/polyfill';
 import Vue from 'vue';
-import './plugins/vuetify'
+import './plugins/vuetify';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.use(VueApollo);
+
+//* Setup ApolloClient
+const defaultClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
+
 Vue.config.productionTip = false;
 
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   render: h => h(App),
