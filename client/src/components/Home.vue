7ea0c304c6e82ec983dac0f2d6ef76182e82448d
Clint Losee <clint.losee@gmail.com>
2019-04-12 12:30:16 -0600

    Set up carousel on home page with apollo query.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,18 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
+  <v-container text-xs-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ cycle: true }" interval="3000">
+        <v-carousel-item
+          v-for="post in getPosts"
+          :key="post._id"
+          :src="post.imageUrl"
+        >
+          <h1 id="carousel__title">{{ post.title }}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
+
+    <!-- <h1>Home</h1>
     <div v-if="$apollo.loading">Loading...</div>
     <ul v-else v-for="post in getPosts" :key="post._id">
       <li>{{ post.title }}</li>
@@ -20,7 +32,7 @@
           <li>{{ post.description }}</li>
         </ul>
       </template>
-    </ApolloQuery>
+    </ApolloQuery> -->
   </v-container>
 </template>
 
@@ -89,3 +101,17 @@ export default {
   }
 };
 </script>
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
